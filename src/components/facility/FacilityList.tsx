39517dcd21d5fb709f47bf0f2f312549d679ede1
rbarisozkal <barisozkal11@gmail.com>
2025-04-19 01:39:36 +0300

    fix(build) build error fixed by moving unnecessary import use

--- src/components/facility/FacilityList.tsx
+++ src/components/facility/FacilityList.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { useEffect } from 'react';
 import { FacilityCard } from './FacilityCard';
 import { useFacilityStore } from '../../store/facilityStore';
 import { useRouter } from "next/navigation";
